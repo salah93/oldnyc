fb2bb5f738636a84f753a2ac6f9011b7d7bfdf2c
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-12-14 15:19:05 -0500

    use argparse instead of deprecated optparse

--- image_fetcher.py
+++ image_fetcher.py
@@ -2,30 +2,30 @@
 #
 # Fetch full images from the library for a random subset of records.
 
-from optparse import OptionParser
-import fetcher
+from argparse import ArgumentParser
 import random
+
+import fetcher
 import record
 
-
 if __name__ == '__main__':
-  parser = OptionParser()
-  parser.add_option("-n", "--num", dest="num",
-                    default=100,
+  parser = ArgumentParser()
+  parser.add_argument("-n", "--num", dest="num",
+                    default=100, type=int,
                     help="How many images to fetch.")
-  parser.add_option("", "--seed", dest="seed", default=12345,
+  parser.add_argument("", "--seed", dest="seed", type=int, default=12345,
                     help="Random number seed.")
-  parser.add_option("-c", "--output_dir", dest="cache_dir", default="images",
+  parser.add_argument("-c", "--output_dir", dest="cache_dir", default="images",
                     help="Images destination dir")
-  parser.add_option("-s", "--secs", dest="secs", default=5,
+  parser.add_argument("-s", "--secs", dest="secs", type=int, default=5,
                     help="Number of seconds to wait between fetches.")
 
-  (options, args) = parser.parse_args()
+  args = parser.parse_args()
 
   rs = record.AllRecords()
-  rand = random.Random(options.seed)
+  rand = random.Random(args.seed)
   rand.shuffle(rs)
-  f = fetcher.Fetcher(options.cache_dir, int(options.secs))
-  for i, r in enumerate(rs[0:int(options.num)]):
-    print "%03d Fetching %s" % (i, r.photo_url)
+  f = fetcher.Fetcher(args.cache_dir, args.secs)
+  for i, r in enumerate(rs[:args.num]):
+    print("%03d Fetching %s" % (i, r.photo_url))
     f.Fetch(r.photo_url)
