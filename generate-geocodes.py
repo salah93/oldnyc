9b6723fc4d8811da5fc311a291bb3b942f9fde7b
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-12-14 15:26:43 -0500

    use argparse instead of optparse

--- generate-geocodes.py
+++ generate-geocodes.py
@@ -8,7 +8,7 @@
 
 import sys
 from collections import defaultdict
-from optparse import OptionParser
+from argparse import ArgumentParser
 import coders.registration
 import record
 import geocoder
@@ -24,74 +24,74 @@ import coders.nyc_parks
 
 
 if __name__ == '__main__':
-  parser = OptionParser()
-  parser.add_option('', '--pickle_path', default=None, dest='pickle_path',
+  parser = ArgumentParser()
+  parser.add_argument('', '--pickle_path', default=None, dest='pickle_path',
                     help='Point to an alternative records.pickle file.')
-  parser.add_option('', '--ids_filter', default='', dest='ids_filter',
+  parser.add_argument('', '--ids_filter', default='', dest='ids_filter',
                     help='Comma-separated list of Photo IDs to consider.')
-  parser.add_option('', '--previous_geocode_json', default='',
+  parser.add_argument('', '--previous_geocode_json', default='',
                     dest='previous_geocode_json',
                     help='Path to a JSON file containing existing geocodes, ' +
                     'as output by this script with --output_format=records.js')
 
-  parser.add_option('-c', '--coders', dest='ok_coders', default='all',
+  parser.add_argument('-c', '--coders', dest='ok_coders', default='all',
                     help='Set to a comma-separated list of coders')
 
-  parser.add_option('-g', '--geocode', dest='geocode', action='store_true',
-                    default=False,
+  parser.add_argument('-g', '--geocode', dest='geocode', action='store_true',
+                    type=bool, default=False,
                     help='Set to geocode all locations. The alternative is ' +
                     'to extract location strings but not geocode them. This ' +
                     'can be useful with --print_records.')
-  parser.add_option('-n', '--use_network', dest='use_network',
-                    action='store_true', default=False,
+  parser.add_argument('-n', '--use_network', dest='use_network',
+                    action='store_true', type=bool, default=False,
                     help='Set this to make the geocoder use the network. The ' +
                     'alternative is to use only the geocache.')
 
-  parser.add_option('-p', '--print_records', dest='print_recs',
-                    action='store_true', default=False,
+  parser.add_argument('-p', '--print_records', dest='print_recs',
+                    action='store_true', type=bool, default=False,
                     help='Set to print out records as they\'re coded.')
-  parser.add_option('-o', '--output_format', default='',
+  parser.add_argument('-o', '--output_format', default='',
                     help='Set to either lat-lons.js, lat-lons-ny.js, records.js'
                     ' or entries.txt to output one of these formats.')
-  parser.add_option('', '--lat_lon_map', default='', dest='lat_lon_map',
+  parser.add_argument('', '--lat_lon_map', default='', dest='lat_lon_map',
                     help='Lat/lon cluster map, built by cluster-locations.py. '
                     'Only used when outputting lat-lons.js')
 
-  (options, args) = parser.parse_args()
+  args = parser.parse_args()
 
-  if options.geocode:
-    g = geocoder.Geocoder(options.use_network, 2)  # 2s between geocodes
+  if args.geocode:
+    g = geocoder.Geocoder(args.use_network, 2)  # 2s between geocodes
   else:
     g = None
 
   geocoders = coders.registration.coderClasses()
   geocoders = [coder() for coder in geocoders]
 
-  if options.ok_coders != 'all':
-    ok_coders = options.ok_coders.split(',')
+  if args.ok_coders != 'all':
+    ok_coders = args.ok_coders.split(',')
     geocoders = [c for c in geocoders if c.name() in ok_coders]
     if len(geocoders) != len(ok_coders):
-      sys.stderr.write('Coder mismatch: %s vs %s\n' % (options.ok_coders, ','.join([c.name() for c in geocoders])))
+      sys.stderr.write('Coder mismatch: %s vs %s\n' % (args.ok_coders, ','.join([c.name() for c in geocoders])))
       sys.exit(1)
 
   # TODO(danvk): does this belong here?
   lat_lon_map = {}
-  if options.lat_lon_map:
-    for line in file(options.lat_lon_map):
+  if args.lat_lon_map:
+    for line in file(args.lat_lon_map):
       line = line.strip()
       if not line: continue
       old, new = line.split('->')
       lat_lon_map[old] = new
 
-  rs = record.AllRecords(path=options.pickle_path)
-  if options.ids_filter:
-    ids = set(options.ids_filter.split(','))
+  rs = record.AllRecords(path=args.pickle_path)
+  if args.ids_filter:
+    ids = set(args.ids_filter.split(','))
     rs = [r for r in rs if r.photo_id() in ids]
 
   # Load existing geocodes, if applicable.
   id_to_located_rec = {}
-  if options.previous_geocode_json:
-    prev_recs = json.load(file(options.previous_geocode_json))
+  if args.previous_geocode_json:
+    prev_recs = json.load(file(args.previous_geocode_json))
     for rec in prev_recs:
       if 'extracted' in rec and 'latlon' in rec['extracted']:
         x = rec['extracted']
@@ -125,7 +125,7 @@ if __name__ == '__main__':
       assert 'address' in location_data
 
       if not g:
-        if options.print_recs:
+        if args.print_recs:
           print '%s\t%s\t%s' % (
               c.name(), r.photo_id(), json.dumps(location_data))
         stats[c.name()] += 1
@@ -149,7 +149,7 @@ if __name__ == '__main__':
       if lat_lon:
         location_data['lat'] = lat_lon[0]
         location_data['lon'] = lat_lon[1]
-        if options.print_recs:
+        if args.print_recs:
           print '%s\t%f,%f\t%s\t%s' % (
               r.photo_id(), lat_lon[0], lat_lon[1], c.name(),
               json.dumps(location_data))
@@ -170,13 +170,13 @@ if __name__ == '__main__':
 
   sys.stderr.write('%5d (total)\n' % successes)
 
-  if options.output_format == 'lat-lons.js':
+  if args.output_format == 'lat-lons.js':
     generate_js.printJson(located_recs, lat_lon_map)
-  if options.output_format == 'lat-lons-ny.js':
+  if args.output_format == 'lat-lons-ny.js':
     generate_js.printJsonNoYears(located_recs, lat_lon_map)
-  elif options.output_format == 'records.js':
+  elif args.output_format == 'records.js':
     generate_js.printRecordsJson(located_recs)
-  elif options.output_format == 'entries.txt':
+  elif args.output_format == 'entries.txt':
     generate_js.printRecordsText(located_recs)
-  elif options.output_format == 'locations.txt':
+  elif args.output_format == 'locations.txt':
     generate_js.printLocations(located_recs)
